87162c1f14443e26499c946bdc61b18842e4055a
Dony Navas <donyhenry@gmail.com>
2023-01-31 22:51:52 -0600

    Inicio

--- start.php
+++ start.php
@@ -1,7 +1,20 @@
 <?php
 session_start();
 
-if(empty($usuario =!0)){
-print("Welcome to System process");
-}
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inventario Ver 1.0</title>
+</head>
+<body>
+    
+
+
+
+</body>
+</html>
