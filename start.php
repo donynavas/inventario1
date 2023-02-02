85b379c6706e914a8987120294adc3214bfa509e
Dony Navas <donyhenry@gmail.com>
2023-02-02 13:30:04 -0600

    actualizacion

--- start.php
+++ start.php
@@ -9,10 +9,20 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/login.css">
     <title>Inventario Ver 1.0</title>
 </head>
 <body>
-    
+<div id="contenedor">
+    <div id="centro">
+        <div id="login">
+            <div class="titulo">Inventario</div>
+            
+            <form id="formulario">
+                <input type="text" name="usuario" id="usuario">
+                <input type="text" name="password" id="password">
+                <input type="submit" title="Entrar" name="Entrar" value="Entrar">
+            </form>  
 
 
 
